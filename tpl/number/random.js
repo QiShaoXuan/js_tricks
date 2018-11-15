765df8dfce3657f497151eb6ea8427de616f629a
QiShaoXuan <474021214@qq.com>
2018-11-15 17:12:31 +0800

    add random

--- tpl/number/random.js
+++ tpl/number/random.js
@@ -1,10 +1,10 @@
 function randomNum(min, max) {
   switch (arguments.length) {
     case 1:
-      return parseInt(Math.random() * min + 1, 10);
+      return parseInt(Math.random() * min + 1, 10)
     case 2:
-      return parseInt(Math.random() * (max - minNum + 1) + min, 10);
+      return parseInt(Math.random() * (max - min + 1) + min, 10)
     default:
-      return 0;
+      return 0
   }
 }
